360e24f6a3eb21daf8c1ef019d9c9e04017c42b0
MaxSDemin <maximsd3@gmail.com>
2023-09-20 00:01:50 +0300

    task1

--- lab2/lab2.c
+++ lab2/lab2.c
@@ -4,15 +4,30 @@
 void bench(int r1, int c1, int r2, int c2) {
     int i, j, k;
 
-    int** a = (int**)malloc(r1 * r2 * sizeof(int*));
-    double(*b) = malloc(c1 * c2 * sizeof(double(b)));
-    double(*result)[r1][c2] = malloc(sizeof * result);
+    int** a = (int**)malloc(r1 * sizeof(int*));
+    for (int i = 0; i < r1; i++)
+    {
+        a[i] = (int*)malloc(r2 * sizeof(int));
+    }
+
+    int** b = (int**)malloc(c1 * sizeof(int*));
+    for (int i = 0; i < r1; i++)
+    {
+        b[i] = (int*)malloc(c2 * sizeof(int));
+    }
+
+    int** result = (int**)malloc(c1 * sizeof(int*));
+    for (int i = 0; i < r1; i++)
+    {
+        result[i] = (int*)malloc(c2 * sizeof(int));
+    }
+
 
     for (i = 0; i < r1; ++i)
     {
         for (j = 0; j < c1; ++j)
         {
-            (*a)[i][j] = rand() % 100 + 1;
+            a[i][j] = rand() % 100 + 1;
         }
     }
 
@@ -20,7 +35,7 @@ void bench(int r1, int c1, int r2, int c2) {
     {
         for (j = 0; j < c2; ++j)
         {
-            (*b)[i][j] = rand() % 100 + 1;
+            b[i][j] = rand() % 100 + 1;
         }
     }
 
@@ -28,7 +43,7 @@ void bench(int r1, int c1, int r2, int c2) {
     {
         for (j = 0; j < c2; ++j)
         {
-            (*result)[i][j] = 0;
+            result[i][j] = 0;
         }
     }
     clock_t begin1 = clock();
@@ -37,12 +52,17 @@ void bench(int r1, int c1, int r2, int c2) {
         for (j = 0; j < c2; ++j)
             for (k = 0; k < c1; ++k)
             {
-                (*result)[i][j] += (*a)[i][k] * (*b)[k][j];
+                result[i][j] += a[i][k] * b[k][j];
             }
 
     clock_t end1 = clock();
     double time_taken = (double)(end1 - begin1) / CLOCKS_PER_SEC;
     printf("\n \nfunction took %f seconds to execute \n", time_taken);
+
+    for (int i = 0; i < r1; i++)
+    {
+        free (a[i]);
+    }
     free(a);
     free(b);
     free(result);

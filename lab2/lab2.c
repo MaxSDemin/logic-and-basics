c157f2c56a02cc2e2d3637d327659576ffc5e848
MaxSDemin <maximsd3@gmail.com>
2023-09-20 22:47:24 +0300

    task2

--- lab2/lab2.c
+++ lab2/lab2.c
@@ -78,13 +78,62 @@ void bench(int r1, int c1, int r2, int c2) {
     free(result);
 }
 
+void shell(int* items, int count)
+{
+
+    int i, j, gap, k;
+    int x, a[5];
+
+    a[0] = 9; a[1] = 5; a[2] = 3; a[3] = 2; a[4] = 1;
+
+    for (k = 0; k < 5; k++) {
+        gap = a[k];
+        for (i = gap; i < count; ++i) {
+            x = items[i];
+            for (j = i - gap; (x < items[j]) && (j >= 0); j = j - gap)
+                items[j + gap] = items[j];
+            items[j + gap] = x;
+        }
+    }
+}
+
+void qs(int* items, int left, int right) //вызов функции: qs(items, 0, count-1);
+{
+    int i, j;
+    int x, y;
+
+
+
+    i = left; j = right;
+
+    /* выбор компаранда */
+    x = items[(left + right) / 2];
+
+    do {
+        while ((items[i] < x) && (i < right)) i++;
+        while ((x < items[j]) && (j > left)) j--;
+
+        if (i <= j) {
+            y = items[i];
+            items[i] = items[j];
+            items[j] = y;
+            i++; j--;
+        }
+    } while (i <= j);
+
+    if (left < j) qs(items, left, j);
+    if (i < right) qs(items, i, right);
+}
+
+
 int main()
 {
-    bench(100, 100, 100, 100);
+    /*bench(100, 100, 100, 100);
     bench(200, 200, 200, 200);
     bench(400, 400, 400, 400);
     bench(1000, 1000, 1000, 1000);
     bench(2000, 2000, 2000, 2000);
     bench(4000, 4000, 4000, 4000);
-    bench(10000, 10000, 10000, 10000);
+    bench(10000, 10000, 10000, 10000);*/
+
 }

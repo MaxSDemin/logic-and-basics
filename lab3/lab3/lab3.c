93086acca8ec10458767ae1a9abadd2796c2d544
MaxSDemin <maximsd3@gmail.com>
2023-10-04 22:22:13 +0300

    end

--- lab3/lab3/lab3.c
+++ lab3/lab3/lab3.c
@@ -1,15 +1,13 @@
-#include <iostream>
+#define _CRT_SECURE_NO_WARNINGS
 #include <stdio.h>
 #include <stdlib.h>
-#include <time.h>
+#include <string.h>
+#include <locale.h>
 
-
-struct node
-{
+typedef struct node {
 	char inf[256];  // полезная информация
-	struct node* next; // ссылка на следующий элемент 
-};
-
+	struct node* next; // ссылка на следующий элемент
+} node;
 
 struct node* head = NULL, * last = NULL, * f = NULL; // указатели на первый и последний элементы списка
 int dlinna = 0;
@@ -21,23 +19,18 @@ char find_el[256];
 struct node* find(char* name); // функция нахождения элемента
 struct node* get_struct(void); // функция создания элемента
 
-
-
-struct node* get_struct(void)
-{
+struct node* get_struct(void) {
 	struct node* p = NULL;
 	char s[256];
 
-	if ((p = (node*)malloc(sizeof(struct node))) == NULL)  // выделяем память под новый элемент списка
-	{
+	if ((p = calloc(1, sizeof(struct node))) == NULL) {
 		printf("Ошибка при распределении памяти\n");
 		exit(1);
 	}
 
 	printf("Введите название объекта: \n");   // вводим данные
 	scanf("%s", s);
-	if (*s == 0)
-	{
+	if (*s == 0) {
 		printf("Запись не была произведена\n");
 		return NULL;
 	}
@@ -49,55 +42,55 @@ struct node* get_struct(void)
 }
 
 /* Последовательное добавление в список элемента (в конец)*/
-void spstore(void)
-{
+void spstore(void) {
 	struct node* p = NULL;
 	p = get_struct();
-	if (head == NULL && p != NULL)	// если списка нет, то устанавливаем голову списка
-	{
+	if (head == NULL && p != NULL) { // если списка нет, то устанавливаем голову списка
 		head = p;
 		last = p;
 	}
-	else if (head != NULL && p != NULL) // список уже есть, то вставляем в конец
-	{
+	else if (head != NULL && p != NULL) { // список уже есть, то вставляем в конец
 		last->next = p;
 		last = p;
 	}
 	return;
 }
 
+void spstore2(void) {
+	struct node* p = NULL;
+	p = get_struct();
+	if (head == NULL && p != NULL) { // если списка нет, то устанавливаем голову списка
+		head = p;
+		last = p;
+	}
+	else if (head != NULL && p != NULL) { // список уже есть, то вставляем в начало
+		p->next = head;
+		head = p;
+	}
+	return;
+}
 
 /* Просмотр содержимого списка. */
-void review(void)
-{
+void review(void) {
 	struct node* struc = head;
-	if (head == NULL)
-	{
+	if (head == NULL) {
 		printf("Список пуст\n");
 	}
-	while (struc)
-	{
+	while (struc) {
 		printf("Имя - %s, \n", struc->inf);
 		struc = struc->next;
 	}
 	return;
 }
 
-
-
-
 /* Поиск элемента по содержимому. */
-struct node* find(char* name)
-{
+struct node* find(char* name) {
 	struct node* struc = head;
-	if (head == NULL)
-	{
+	if (head == NULL) {
 		printf("Список пуст\n");
 	}
-	while (struc)
-	{
-		if (strcmp(name, struc->inf) == 0)
-		{
+	while (struc) {
+		if (strcmp(name, struc->inf) == 0) {
 			return struc;
 		}
 		struc = struc->next;
@@ -107,61 +100,198 @@ struct node* find(char* name)
 }
 
 /* Удаление элемента по содержимому. */
-void del(char* name)
-{
+void del(char* name) {
 	struct node* struc = head; // указатель, проходящий по списку установлен на начало списка
-	struct node* prev;// указатель на предшествующий удаляемому элемент
+	struct node* prev = NULL; // указатель на предшествующий удаляемому элемент
 	int flag = 0;      // индикатор отсутствия удаляемого элемента в списке
 
-	if (head == NULL) // если голова списка равна NULL, то список пуст
-	{
+	if (head == NULL) { // если голова списка равна NULL, то список пуст
 		printf("Список пуст\n");
 		return;
 	}
 
-	if (strcmp(name, struc->inf) == 0) // если удаляемый элемент - первый
-	{
+	if (strcmp(name, struc->inf) == 0) { // если удаляемый элемент - первый
 		flag = 1;
 		head = struc->next; // установливаем голову на следующий элемент
 		free(struc);  // удаляем первый элемент
 		struc = head; // устанавливаем указатель для продолжения поиска
 	}
-	else
-	{
+	else {
 		prev = struc;
 		struc = struc->next;
 	}
 
-	while (struc) // проход по списку и поиск удаляемого элемента
-	{
-		if (strcmp(name, struc->inf) == 0) // если нашли, то
-		{
+	while (struc) { // проход по списку и поиск удаляемого элемента
+		if (strcmp(name, struc->inf) == 0) { // если нашли, то
 			flag = 1;         // выставляем индикатор
-			if (struc->next)  // если найденный элемент не последний в списке
-			{
+			if (struc->next) { // если найденный элемент не последний в списке
 				prev->next = struc->next; // меняем указатели
-				free(struc);		    // удаляем  элемент
+				free(struc); // удаляем  элемент
 				struc = prev->next; // устанавливаем указатель для продолжения поиска
 			}
-			else			// если найденный элемент последний в списке
-			{
+			else { // если найденный элемент последний в списке
 				prev->next = NULL; // обнуляем указатель предшествующего элемента
-				free(struc);	// удаляем  элемент
+				free(struc); // удаляем  элемент
 				return;
 			}
 		}
-		else / если не нашли, то
-		{
+		else { // если не нашли, то
 			prev = struc; // устанавливаем указатели для продолжения поиска
 			struc = struc->next;
 		}
 	}
 
-	if (flag == 0)				// если флаг = 0, значит нужный элемент не найден
-	{
+	if (flag == 0) { // если флаг = 0, значит нужный элемент не найден
 		printf("Элемент не найден\n");
 		return;
 	}
 
+}
+
+/*
+ *   3333
+ *       3
+ *       3
+ *   3333
+ *       3
+ *       3
+ *   3333
+ */
+
+typedef struct PQobj {
+	node* inf;  // полезная информация
+	int prior;
+} PQobj;
+
+void qp() {
+	printf("нечестная очередь:\n");
+	int i2 = 15;
+
+	int q_size = 0;
+	PQobj* q = NULL;
+
+	for (int i = 1; i <= i2; i++) {
+		printf("выберите действие: 1 - добавить, 2 - просмотр, 3 - удалить (%d/%d)\n", i, i2);
+
+		int act = 0;
+		scanf("%d", &act);
+
+		if (act == 1) {
+			q_size++;
+			q = realloc(q, sizeof(PQobj) * q_size);
+
+			node* inf = get_struct();
+			int prior = 0;
+			printf("Введите приоритет:\n");
+			scanf("%d", &prior);
+
+			q[q_size - 1].inf = inf;
+			q[q_size - 1].prior = prior;
+
+			for (int i3 = 0; i3 < q_size; i3++) {
+				for (int i4 = 0; i4 < q_size; i4++) {
+					if (i3 == i4)
+						continue;
+					PQobj n1 = q[i3];
+					PQobj n2 = q[i4];
+
+					if (n1.prior > n2.prior) {
+						q[i3] = n2;
+						q[i4] = n1;
+					}
+				}
+			}
+		}
+		else if (act == 2) {
+			for (int i3 = 0; i3 < q_size; i3++) {
+				PQobj n1 = q[i3];
+				printf("%s -- приоритет %d\n", n1.inf->inf, n1.prior);
+			}
+		}
+		else if (act == 3) {
+			q_size--;
+			memmove(&q[0], &q[1], sizeof(q) * q_size);
+		}
+		else {
+			printf("нет такого действия\n");
+		}
+
+	}
+
+	for (int i = 0; i < q_size; i++) {
+		free(q[i].inf);
+	}
+	free(q);
+}
+
+void q() {
+	printf("очередь:\n");
+	int i2 = 15;
+	for (int i = 1; i <= i2; i++) {
+		printf("выберите действие: 1 - добавить, 2 - просмотр, 3 - удалить (%d/%d)\n", i, i2);
+
+		int act = 0;
+		scanf("%d", &act);
+
+		if (act == 1) {
+			spstore();
+		}
+		else if (act == 2) {
+			review();
+		}
+		else if (act == 3) {
+			if (head) {
+				printf("Удален: %s\n", head->inf);
+				del(head->inf);
+			}
+			else {
+				printf("Нечего удалять\n");
+			}
+		}
+		else {
+			printf("нет такого действия\n");
+		}
+	}
+	printf("\n");
+}
+
+void stack() {
+	printf("стек:\n");
+	int i2 = 15;
+	for (int i = 1; i <= i2; i++) {
+		printf("выберите действие: 1 - добавить, 2 - просмотр, 3 - удалить (%d/%d)\n", i, i2);
+
+		int act = 0;
+		scanf("%d", &act);
+
+		if (act == 1) {
+			spstore2();
+		}
+		else if (act == 2) {
+			review();
+		}
+		else if (act == 3) {
+			if (head) {
+				printf("Удален: %s\n", head->inf);
+				del(head->inf);
+			}
+			else {
+				printf("Нечего удалять\n");
+			}
+		}
+		else {
+			printf("нет такого действия\n");
+		}
+	}
+	printf("\n");
+}
+
+int main() {
+	setlocale(LC_ALL, "rus");
+	setbuf(stdout, NULL);
+
+	qp();
+	q();
+	stack();
 
 }

219218df349d8aa115d079f1a3d85d1c34db7a0e
MaxSDemin <maximsd3@gmail.com>
2023-10-12 18:32:53 +0300

    end

--- lab5/lab5.c
+++ lab5/lab5.c
@@ -1,25 +1,12 @@
+#define _CRT_SECURE_NO_WARNINGS
 #include <stdio.h>
 #include <stdlib.h>
 #include <time.h>
 
-#define SIZE 6 // Размер матрицы смежности
-
-void generateMatrix(int matrix[SIZE][SIZE]) {
-    srand(time(NULL)); // Инициализация генератора случайных чисел
-
-    for (int i = 0; i < SIZE; i++) {
-        for (int j = i + 1; j < SIZE; j++) {
-            int random = rand() % 2; // Генерация случайного числа 0 или 1
-            matrix[i][j] = random;
-            matrix[j][i] = random; // Заполнение элементов и их симметричных пар
-        }
-    }
-}
-
-int calc(int matrix[SIZE][SIZE]) {
+int calc(int** matrix,int size) {
     int N = 0;
-    for (int i = 0; i < SIZE; i++) {
-        for (int j = 0; j < SIZE; j++) {
+    for (int i = 0; i < size; i++) {
+        for (int j = 0; j < size; j++) {
             if (matrix[i][j] == 1) {
                 N += 1;
             }
@@ -29,57 +16,120 @@ int calc(int matrix[SIZE][SIZE]) {
     return N;
 }
 
-int grafCount(int arr[SIZE][SIZE]) {
+int grafCount(int** arr, int size) {
     int sum;
-    for (int i = 0; i < SIZE; i++)
+    for (int i = 0; i < size; i++)
     {
         sum = 0;
-        for (int j = 0; j < SIZE; j++)
+        for (int j = 0; j < size; j++)
         {
             sum += arr[i][j];
         }
         if (sum == 1) {
-            printf("\n%d string: %d  vershina - koncevaya\n", i + 1, sum);
+            printf("%d string: %d  vershina - koncevaya\n", i + 1, sum);
         }
         if (sum == 0) {
-            printf("\n%d string: %d  vershina - izolirovanaya\n", i + 1, sum);
+            printf("%d string: %d  vershina - izolirovanaya\n", i + 1, sum);
         }
-        if (sum == SIZE - 1) {
-            printf("\n%d string: %d  vershina - dominiruyshaya\n", i + 1, sum);
+        if (sum == size - 1) {
+            printf("%d string: %d  vershina - dominiruyshaya\n", i + 1, sum);
         }
     }
 }
 
-void printMatrix(int matrix[SIZE][SIZE]) {
-    for (int i = 0; i < SIZE; i++) {
-        for (int j = 0; j < SIZE; j++) {
+void printMatrix(int** matrix, int size) {
+    for (int i = 0; i < size; i++) {
+        for (int j = 0; j < size; j++) {
             printf("%d ", matrix[i][j]);
         }
         printf("\n");
     }
 }
 
-int GenerateIncidenceMatrix(int matrix[SIZE][SIZE], int countOfEdges)
-{
-    int** incMatrix = (int**)malloc(SIZE * sizeof(int*));
-    for (int i = 0; i < SIZE; i++)
+int sizeOfIncGraph(int** incMatrix, int countOfEdges) {
+    int count = 0;
+    for (int i = 0; i < countOfEdges; i++) {
+        count++;
+    }
+    return count;
+}
+
+int incGrafCount(int** arr, int countOfEdges, int size) {
+    int sum;
+    for (int i = 0; i < size; i++)
     {
-        incMatrix[i] = (int*)malloc(countOfEdges * sizeof(int));
+        sum = 0;
+        for (int j = 0; j < countOfEdges; j++)
+        {
+            sum += arr[i][j];
+        }
+        if (sum == 1) {
+            printf("%d string: %d  vershina - koncevaya\n", i + 1, sum);
+        }
+        if (sum == 0) {
+            printf("%d string: %d  vershina - izolirovanaya\n", i + 1, sum);
+        }
+        if (sum == size - 1) {
+            printf("%d string: %d  vershina - dominiruyshaya\n", i + 1, sum);
+        }
+    }
+}
+
+int main() {
+    //Часть 1
+    int size = 0;
+    printf("Enter a count of headers: ");
+    scanf(" %d", &size);
+
+    srand(time(NULL)); // Инициализация генератора случайных чисел
+
+    int** matrix = (int**)malloc(size * sizeof(int*));
+    for (int i = 0; i < size; i++)
+    {
+        matrix[i] = (int*)malloc(size * sizeof(int));
+    }
+    for (int i = 0; i < size; i++) {
+        for (int j = 0; j < size; j++) {
+            matrix[i][j] = 0;
+        }
+    }
+
+
+    for (int i = 0; i < size; i++) {
+        for (int j = i+1; j < size; j++) {
+            int random = rand() % 2; // Генерация случайного числа 0 или 1
+            matrix[i][j] = random;
+            matrix[j][i] = random; // Заполнение элементов и их симметричных пар
+        }
+    }
+    printMatrix(matrix, size);
+    grafCount(matrix, size);
+
+    int count = calc(matrix,size);
+    printf("Size of graf = %d\n\n", count);
+
+
+    //Часть 2
+    //int incMatrix = GenerateIncidenceMatrix(matrix, count);
+    int** incMatrix = (int**)malloc(size * sizeof(int*));
+    for (int i = 0; i < size; i++)
+    {
+        incMatrix[i] = (int*)malloc(count * sizeof(int));
     }
 
     int curEdge = 0;
 
-    for (int i = 0; i < SIZE; i++)
+    for (int i = 0; i < size; i++)
     {
-        for (int j = 0; j < countOfEdges; j++)
+        for (int j = 0; j < count; j++)
         {
             incMatrix[i][j] = 0;
         }
     }
 
-    for (int i = 0; i < SIZE; i++)
+    for (int i = 0; i < size; i++)
     {
-        for (int j = i; j < SIZE; j++)
+        for (int j = i; j < size; j++)
         {
             if (matrix[i][j] == 1)
             {
@@ -90,70 +140,14 @@ int GenerateIncidenceMatrix(int matrix[SIZE][SIZE], int countOfEdges)
         }
     }
 
-    for (int i = 0; i < SIZE; i++) {
-        for (int j = 0; j < countOfEdges; j++) {
+    for (int i = 0; i < size; i++) {
+        for (int j = 0; j < count; j++) {
             printf("%d ", incMatrix[i][j]);
         }
         printf("\n");
     }
-    return **incMatrix;
-}
-
-int sizeOfIncGraph(int** incMatrix, int countOfEdges) {
-    int N = 0;
-    int count = 0;
-    for (int i = 0; i < countOfEdges; i++) {
-        for (int j = 0; i < SIZE; i++) {
-            if (incMatrix[j][i] == 1) N++;
-        }
-        if (N == 2) count++;
-        N = 0;
-    }
-    return count;
-}
-
-int incGrafCount(int** arr, int count) {
-    int sum;
-    for (int i = 0; i < SIZE; i++)
-    {
-        sum = 0;
-        for (int j = 0; j < count; j++)
-        {
-            sum += arr[i][j];
-        }
-        if (sum == 1) {
-            printf("\n%d string: %d  vershina - koncevaya\n", i + 1, sum);
-        }
-        if (sum == 0) {
-            printf("\n%d string: %d  vershina - izolirovanaya\n", i + 1, sum);
-        }
-        if (sum == SIZE - 1) {
-            printf("\n%d string: %d  vershina - dominiruyshaya\n", i + 1, sum);
-        }
-    }
-}
-
-int main() {
-    int matrix[SIZE][SIZE] = { 0 };
-
-    //Часть 1
-    generateMatrix(matrix);
-    printMatrix(matrix);
-    grafCount(matrix);
-
-    int count = calc(matrix);
-    printf("Size of graf = %d\n\n", count);
-
-
-    //Часть 2
-    int** incMatrix = (int**)malloc(SIZE * sizeof(int*));
-    for (int i = 0; i < SIZE; i++)
-    {
-        incMatrix[i] = (int*)malloc(count * sizeof(int));
-    }
-    **incMatrix = GenerateIncidenceMatrix(matrix, count);
-    printf("Size of graf = %d\n\n", sizeOfIncGraph(**incMatrix, count));
-    incGrafCount(**incMatrix, count);
+    printf("size of graf = %d\n\n", sizeOfIncGraph(incMatrix, count));
+    incGrafCount(incMatrix, count, size);
 
     return 0;
 }

609821c60efd2fe3d7d5b147a56a82175d421573
MaxSDemin <maximsd3@gmail.com>
2023-10-27 16:39:57 +0300

    end

--- lab8/lab8.cpp
+++ lab8/lab8.cpp
@@ -1,5 +1,6 @@
 #define _CRT_SECURE_NO_WARNINGS
 
+#include <queue>
 #include <stdio.h>
 #include <stdlib.h>
 #include <time.h>
@@ -7,7 +8,7 @@
 #include <windows.h>
 #include <stdbool.h>
 
-#define NO_PRINT
+//#define NO_PRINT
 
 typedef struct node {
 	int x;
@@ -91,7 +92,7 @@ int main() {
 		if (done)
 			break;
 		// 1.3.
-		node* q_first = calloc(1, sizeof(struct node)); // 2.1.
+		node* q_first = (node*) calloc(1, sizeof(struct node)); // 2.1.
 		node* q_last = q_first;
 
 		q_first->x = i; // 2.2.
@@ -105,6 +106,13 @@ int main() {
 			if (q_first == NULL)
 				q_last = NULL;
 
+			done = true;
+			for (int i2 = 1; i2 < size; i2++)
+				if (matrix[i][i2] == 1)
+					done = false;
+			if (done)
+				break;
+
 			#ifndef NO_PRINT
 			printf("%d\n", f->x + 1); // 2.7.
 			#endif
@@ -112,11 +120,11 @@ int main() {
 			for (int i2 = 1; i2 < size; i2++) { // 2.8.
 				if (matrix[i][i2] == 1 && num[i2] == false) { // 2.9.
 					if (q_last) {
-						q_last->next = calloc(1, sizeof(struct node)); // 2.11
+						q_last->next = (node*) calloc(1, sizeof(struct node)); // 2.11
 						q_last = q_last->next;
 					}
 					else {
-						q_first = calloc(1, sizeof(struct node)); // 2.11
+						q_first = (node*) calloc(1, sizeof(struct node)); // 2.11
 						q_last = q_first;
 					}
 
@@ -125,8 +133,11 @@ int main() {
 
 					num[i2] = true; // 2.12.
 				}
+				
 			}
 		}
+		if (done)
+			break;
 
 	}
 	clock_t time_end = clock();
@@ -135,7 +146,65 @@ int main() {
 	setbuf(stdout, NULL);
 
 	double diff = time_end - time_start;
-	printf("time elapsed: %fms", diff);
+	printf("time elapsed: %fms\n", diff);
+
+
+	free(num);
+	setvbuf(stdout, NULL, _IOFBF, 16384);
+	num = (int*)calloc(size, sizeof(int)); // 1.1
+	
+	time_start = clock();
+	while (true) { // 1.2.
+		bool done = true;
+		int i = 0;
+		for (; i < size; i++) {
+			if (num[i] == 0) {
+				done = false;
+				break;
+			}
+		}
+		if (done)
+			break;
+		// 1.3.
+
+		std::queue<int> q;
+
+		q.push(i);
+		num[i] = true; // 2.3.
+
+		while (!q.empty()) { // 2.4.
+			int x = q.front();
+			q.pop();
+
+			done = true;
+			for (int i2 = 1; i2 < size; i2++)
+				if (matrix[i][i2] == 1)
+					done = false;
+			if (done)
+				break;
+
+			#ifndef NO_PRINT
+			printf("%d\n", x + 1); // 2.7.
+			#endif
+
+			for (int i2 = 1; i2 < size; i2++) { // 2.8.
+				if (matrix[i][i2] == 1 && num[i2] == false) { // 2.9.
+					q.push(i2);
+					num[i2] = true; // 2.12.
+				}
+			}
+		}
+		if (done)
+			break;
+
+	}
+	time_end = clock();
+
+	fflush(stdout);
+	setbuf(stdout, NULL);
+
+	diff = time_end - time_start;
+	printf("time elapsed: %fms\n", diff);
 
 
 	for (int i = 0; i < size; i++)
